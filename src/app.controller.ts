fb645c6c3ea2097457c318009ac7423240a5a978
marcio.santos <marcio.santos@redfox.tech>
2021-05-02 16:40:44 -0300

    first emmiter working

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport
+} from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
 @Controller('api/v1')
 export class AppController {
   private logger = new Logger(AppController.name);
-  constructor() {}
+  private clientAdminBackend: ClientProxy;
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://user:5AF2R2vhyS5k@54.157.189.99:5672/smartranking'],
+        queue: 'admin-backend'
+      }
+    });
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+    return await this.clientAdminBackend.emit(
+      'criar-categoria',
+      criarCategoriaDto
+    );
   }
 }
